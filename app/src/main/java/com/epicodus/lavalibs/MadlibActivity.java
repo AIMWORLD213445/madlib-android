6ed0487594fe1530709c1cdf89432a68d463dad5
Brian Dutz and Jeremy Fryd <brian@>
2016-11-15 14:26:16 -0800

    fix bugs so it works

--- app/src/main/java/com/epicodus/lavalibs/MadlibActivity.java
+++ app/src/main/java/com/epicodus/lavalibs/MadlibActivity.java
@@ -1,13 +1,23 @@
 package com.epicodus.lavalibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MadlibActivity extends AppCompatActivity {
+    private TextView madlib;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_madlib);
+        madlib = (TextView) findViewById(R.id.madlib);
+        Intent intent = getIntent();
+        String verb = intent.getStringExtra("verb");
+        String adjective = intent.getStringExtra("adjective");
+        String noun = intent.getStringExtra("noun");
+
+        madlib.setText("your words:" + verb + adjective + noun);
     }
 }
--- app/src/main/java/com/epicodus/lavalibs/MainActivity.java
+++ app/src/main/java/com/epicodus/lavalibs/MainActivity.java
@@ -1,13 +1,40 @@
 package com.epicodus.lavalibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button button;
+    private EditText editText1;
+    private EditText editText2;
+    private EditText editText3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText1 = (EditText) findViewById(R.id.editText1);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        editText3 = (EditText) findViewById(R.id.editText3);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String verb = editText1.getText().toString();
+                String adjective = editText2.getText().toString();
+                String noun = editText3.getText().toString();
+                Intent intent = new Intent(MainActivity.this, MadlibActivity.class);
+                intent.putExtra("verb", verb);
+                intent.putExtra("adjective", adjective);
+                intent.putExtra("noun", noun);
+                startActivity(intent);
+            }
+        });
+
     }
 }
